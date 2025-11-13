51c7712bfb3f01b61b9d96a864605ef02233364e
Elanio <162119025+Elaniio@users.noreply.github.com>
2025-11-13 12:33:18 -0300

    Delete D1-Calculo-scipt.js

--- D1-Calculo-scipt.js
+++
@@ -1,6 +0,0 @@
-const peso = parseFloat(prompt("Digite seu peso em kg:"));
-const altura = parseFloat(prompt("Digite sua altura em metros:"));
-const imc = peso / (altura * altura);
-
-console.log("Seu IMC é: " + imc.toFixed(2));
-alert("Seu IMC é: " + imc.toFixed(2));
